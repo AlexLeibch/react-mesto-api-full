852a16819992d6df9286cfe987f8712f3053bcbb
Alexanrd Leibch <71503887+DeadlyVi@users.noreply.github.com>
2021-10-28 08:39:59 +0200

    fix looger

--- backend/middlewares/logger.js
+++ backend/middlewares/logger.js
@@ -4,7 +4,7 @@ const expressWinston = require('express-winston');
 // логгер запросов
 const requestLogger = expressWinston.logger({
   transports: [
-    new winston.transports.File({filename: 'request.log'}),
+    new winston.transports.File({ filename: 'request.log' }),
   ],
   format: winston.format.json(),
 });
@@ -12,7 +12,7 @@ const requestLogger = expressWinston.logger({
 // логгер ошибок
 const errorLogger = expressWinston.errorLogger({
   transports: [
-    new winston.transports.File({ filename: 'error.log'})
+    new winston.transports.File({ filename: 'error.log' }),
   ],
   format: winston.format.json(),
 });
@@ -20,6 +20,4 @@ const errorLogger = expressWinston.errorLogger({
 module.exports = {
   requestLogger,
   errorLogger,
-}
-
-// beb
+};
